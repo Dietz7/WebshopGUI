b5e78ce440efea7c9e9617c764ec8fd143cf732c
Dietz7 <yuliya.arkhi@gmail.com>
2025-03-08 15:44:47 +0100

    Updated User model

--- src/main/java/com/webshop/models/User.java
+++ src/main/java/com/webshop/models/User.java
@@ -42,4 +42,11 @@ public class User {
     public String getPassword() {
         return password;
     }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "password='" + password + '\'' +
+                '}';
+    }
 }
