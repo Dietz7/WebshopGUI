ba554d4094baf903374dcc47127d0de9d8a95bfc
Dietz7 <yuliya.arkhi@gmail.com>
2025-03-11 22:02:44 +0100

    added data

---
+++ src/main/java/com/webshop/data/UserData.java
@@ -0,0 +1,11 @@
+package com.webshop.data;
+
+public class UserData {
+
+    public static final String FIRSTNAME = "Juli";
+
+    public static final String LASTNAME = "Tester";
+    public static final String EMAIL = "juli777@gmail.com";
+    public static final String PASSWORD = "Qwerty1!$";
+
+}
